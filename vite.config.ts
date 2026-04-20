5a178a80c57d8ff4ab280aed3b88a42af517d28f
werasak.m <jokerwwe0542@gmail.com>
2026-04-20 23:02:59 +0700

    Fix base path to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/resume/',
+  base: '/werasak-resume/',
   plugins: [
     vue(),
     vueJsx(),
